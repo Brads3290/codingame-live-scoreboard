59edaafd236b5ea392bc5cc8384e402e13c09135
github@brads3290.com <rS329sRB>
2021-01-11 09:06:32 +1100

    Updated boilerplate code to export Handle

--- _boilerplate/_main.go
+++ _boilerplate/_main.go
@@ -1,12 +1,11 @@
 package _boilerplate
 
-func handle(ctx context.Context, request events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
-	return codezone_util.UnifyLambdaResponse(ctx, func () (sts int, resp interface{}, err error) {
+func Handle(ctx context.Context, request events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	return codezone_util.UnifyLambdaResponse(ctx, func() (sts int, resp interface{}, err error) {
 		return
 	})
 }
 
 func main() {
-	lambda.Start(handle)
+	lambda.Start(Handle)
 }
-
