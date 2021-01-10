6af3886c419e39d0c4baac8a9701d27297baeb32
github@brads3290.com <rS329sRB>
2021-01-11 09:09:59 +1100

    Changed logger name

--- api/putevent/handler/putevent.go
+++ api/putevent/handler/putevent.go
@@ -8,14 +8,14 @@ import (
 	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/google/uuid"
-	logger "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
 // PUT /putevent
 // Body: { "name": "Event_Name" }
 func Handle(ctx context.Context, request events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
 	return codezone_util.UnifyLambdaResponse(ctx, func() (sts int, resp interface{}, err error) {
-		log := logger.WithField(constants.API_LOGGER_KEY, "putevent")
+		log := logrus.WithField(constants.API_LOGGER_KEY, "putevent")
 
 		// Get the name from the request body
 		body := struct {
