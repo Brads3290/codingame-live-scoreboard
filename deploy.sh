2546a85ebd5050443502dd385f1338324aa6d16d
github@brads3290.com <rS329sRB>
2021-01-12 10:50:41 +1100

    Deployment script deploys to all lambdas now

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/zsh
 
 
-aws lambda update-function-code --function-name codingame-live-scoreboard-getevent --zip-file fileb://bin/api/getevent.zip
+#aws lambda update-function-code --function-name codingame-live-scoreboard-getevent --zip-file fileb://bin/api/getevent.zip
 
-## Deploy
-#for d in bin/api/*.zip ; do
-#  d2="${d:gs/bin\/api\//}"
-#  d2="${d2:gs/.zip/}"
-#
-#  fn_name="codingame-live-scoreboard-""$d2"
-#
-#  aws lambda update-function-code --function-name "$fn_name" --zip-file "fileb://""$d"
-#done
+# Deploy
+for d in bin/api/*.zip ; do
+  d2="${d:gs/bin\/api\//}"
+  d2="${d2:gs/.zip/}"
+
+  fn_name="codingame-live-scoreboard-""$d2"
+
+  echo "Deploying $fn_name"
+  aws lambda update-function-code --function-name "$fn_name" --zip-file "fileb://""$d" > /dev/null
+done
