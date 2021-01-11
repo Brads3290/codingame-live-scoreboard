761f7f9d31bbffcd03485c07584ca10d63682725
github@brads3290.com <rS329sRB>
2021-01-12 10:25:31 +1100

    Updated build scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
 #!/bin/zsh
 
-ls ./api
+# Clean up
+rm -r bin/api/
+
+# Build
+for d in api/*/ ; do
+  buildPath="./""$d""main"
+  outPath="./bin/""${d%?}"
+
+  echo "Building $buildPath to $outPath"
+
+  env GOOS=linux go build -o "$outPath" "$buildPath"
+done
+
+# Package
+for d in bin/api/* ; do
+  zip "$d.zip" "$d"
+done
---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+
+aws lambda update-function-code --function-name codingame-live-scoreboard-getevent --zip-file fileb://bin/api/getevent.zip
+
+## Deploy
+#for d in bin/api/*.zip ; do
+#  d2="${d:gs/bin\/api\//}"
+#  d2="${d2:gs/.zip/}"
+#
+#  fn_name="codingame-live-scoreboard-""$d2"
+#
+#  aws lambda update-function-code --function-name "$fn_name" --zip-file "fileb://""$d"
+#done
