04fa0a5da4af66c77da1fa08d3ecd8eba0be2edc
github@brads3290.com <rS329sRB>
2021-01-29 20:14:15 +1100

    Finished debugging server

--- _testing/server/main/main.go
+++ _testing/server/main/main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	deleteevent "codingame-live-scoreboard/api/deleteevent/handler"
-	deleteround "codingame-live-scoreboard/api/deleteround/handler"
-	getevent "codingame-live-scoreboard/api/getevent/handler"
-	getevents "codingame-live-scoreboard/api/getevents/handler"
-	putevent "codingame-live-scoreboard/api/putevent/handler"
-	putround "codingame-live-scoreboard/api/putround/handler"
+	event "codingame-live-scoreboard/api/event/handler"
+	round "codingame-live-scoreboard/api/round/handler"
+	scoreboard "codingame-live-scoreboard/api/scoreboard/handler"
 	stats "codingame-live-scoreboard/api/stats/handler"
-	updateevent "codingame-live-scoreboard/api/updateevent/handler"
 	"context"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
@@ -17,8 +13,11 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
+
+var handlerMap = make(map[string]func(writer http.ResponseWriter, request *http.Request))
 
 func main() {
 
@@ -26,32 +25,17 @@ func main() {
 	// 1. Serving static files
 	// 2. Serving the API
 
-	http.HandleFunc("/", handleStatic)
-
 	// List API endpoints
-	http.HandleFunc("/api", createHandler(deleteevent.Handle))
-	http.HandleFunc("/api", createHandler(deleteround.Handle))
-	http.HandleFunc("/api", createHandler(getevent.Handle))
-	http.HandleFunc("/api", createHandler(getevents.Handle))
-	http.HandleFunc("/api", createHandler(putevent.Handle))
-	http.HandleFunc("/api", createHandler(putround.Handle))
-	http.HandleFunc("/api", createHandler(stats.Handle))
-	http.HandleFunc("/api", createHandler(updateevent.Handle))
+	handlerMap["/api/stats"] = createHandler(stats.Handle, regexp.MustCompile(`/api/stats/(?P<event_id>[\w-]+)`))
+	handlerMap["/api/scoreboard"] = createHandler(scoreboard.Handle, regexp.MustCompile(`/api/scoreboard/(?P<event_id>[\w-]+)`))
+	handlerMap["/api/event"] = createHandler(event.Handle, regexp.MustCompile(`/api/event/(?P<event_id>[\w-]+)`))
+	handlerMap["/api/round"] = createHandler(round.Handle, regexp.MustCompile(`/api/round/(?P<event_id>[\w-]+)`), regexp.MustCompile(`/api/round/(?P<event_id>[\w-]+)/(?P<round_id>[\w-]+)`))
+
+	http.HandleFunc("/", handleStatic)
 
 	err := http.ListenAndServe(":8080", nil)
 
 	fmt.Println(err)
-}
-
-func createHandler(apiHandler func(ctx context.Context, request events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error)) func(writer http.ResponseWriter, request *http.Request) {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		// Within here, transform request into events.APIGatewayV2HTTPRequest
-		// When we get a return value, write it to the writer
-	}
-}
-
-func handleApi(writer http.ResponseWriter, request *http.Request) {
-
 }
 
 // HandleStatic retrieves static files based on the path, and returns them as-is.
@@ -67,6 +51,15 @@ func handleStatic(writer http.ResponseWriter, request *http.Request) {
 		}
 	}()
 
+	// First, check if the path matches any in handlerMap, otherwise serve from static.
+	path := request.URL.Path
+	for k, v := range handlerMap {
+		if strings.Index(path, k) == 0 {
+			v(writer, request)
+			return
+		}
+	}
+
 	fmt.Printf("%s %s\n", request.Method, request.URL.String())
 
 	// Only supports get requests
@@ -74,7 +67,6 @@ func handleStatic(writer http.ResponseWriter, request *http.Request) {
 		panic("Unsupported HTTP Method")
 	}
 
-	path := request.URL.Path
 	wd, err := os.Getwd()
 	if err != nil {
 		panic(err)
@@ -115,4 +107,104 @@ func handleStatic(writer http.ResponseWriter, request *http.Request) {
 	}
 
 	return
+}
+
+func createHandler(apiHandler func(ctx context.Context, request events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error), re ...*regexp.Regexp) func(writer http.ResponseWriter, request *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				writer.WriteHeader(500)
+				_, err := writer.Write([]byte(fmt.Sprint(r)))
+				if err != nil {
+					fmt.Println("Error writing response: ", err)
+				}
+			}
+		}()
+
+		// Within here, transform request into events.APIGatewayV2HTTPRequest
+		// When we get a return value, write it to the write
+
+		defer request.Body.Close()
+
+		// Get the body for the awsReqData
+		b, err := ioutil.ReadAll(request.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		awsReqData := events.APIGatewayV2HTTPRequest{
+			RequestContext: events.APIGatewayV2HTTPRequestContext{
+				HTTP: events.APIGatewayV2HTTPRequestContextHTTPDescription{
+					Method:   request.Method,
+					Path:     request.URL.Path,
+					Protocol: request.URL.Scheme,
+				},
+			},
+			QueryStringParameters: convertQueryStringParams(request.URL.Query()),
+			Body:                  string(b),
+			RawPath:               request.URL.Path,
+			RawQueryString:        request.URL.RawQuery,
+			PathParameters:        getPathParameters(request.URL.Path, re...),
+		}
+
+		awsRespData, err := apiHandler(context.TODO(), awsReqData)
+		if err != nil {
+			panic(err)
+		}
+
+		for k, v := range awsRespData.Headers {
+			writer.Header().Set(k, v)
+		}
+
+		writer.WriteHeader(awsRespData.StatusCode)
+
+		_, err = writer.Write([]byte(awsRespData.Body))
+		if err != nil {
+			panic(err)
+		}
+
+		return
+	}
+}
+
+func reSubMatchMap(r *regexp.Regexp, str string) map[string]string {
+	match := r.FindStringSubmatch(str)
+	if match == nil {
+		return nil
+	}
+
+	subMatchMap := make(map[string]string)
+	for i, name := range r.SubexpNames() {
+		if i != 0 {
+			subMatchMap[name] = match[i]
+		}
+	}
+
+	return subMatchMap
+}
+
+func convertQueryStringParams(m map[string][]string) map[string]string {
+	ret := make(map[string]string)
+
+	for k, v := range m {
+		ret[k] = strings.Join(v, ",")
+	}
+
+	return ret
+}
+
+func getPathParameters(p string, regexes ...*regexp.Regexp) map[string]string {
+
+	m := make(map[string]string)
+	for _, r := range regexes {
+		matches := reSubMatchMap(r, p)
+
+		if matches != nil {
+			for k, v := range matches {
+				m[k] = v
+			}
+		}
+	}
+
+	return m
 }
